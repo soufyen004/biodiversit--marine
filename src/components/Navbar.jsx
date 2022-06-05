a6358c72952506a0087015b14658c00fd8e827bc
soufyen004 <soufyen004@gmail.com>
2022-06-05 21:23:42 +0100

    'recode'

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,10 +17,7 @@ function RespNav() {
           <Link to="/" className="navbar-brand img-fluid">
             <img src={Intellcap} alt="Logo" width="200" class="d-none d-md-inline-block p-0" />
           </Link>
-          <a className="navbar-brand img-fluid p-0 d-lg-none" href="#">
-            <img src={Logo} alt="" width="150" class="d-inline-block p-0" />
-          </a>
-          <div class="dropdown dropdown-menu-end d-flex mx-2 ms-auto">
+          <div class="dropdown dropdown-menu-end d-flex me-auto">
             <button class="btn btn-transparent dropdown-toggle ms-auto" type="button" id="dropdownMenu2" data-bs-toggle="dropdown" aria-expanded="false">
               <i className='fa-solid fa-language fs-4'></i>
             </button>
@@ -29,11 +26,15 @@ function RespNav() {
               <li><button class="dropdown-item text-center" type="button"><img width={'20px'} src={frFlag}/> FR</button></li>
             </ul>
           </div>
+          <a className="ms-auto navbar-brand img-fluid p-0 d-lg-none" href="#">
+            <img src={Logo} alt="" width="190" class="d-inline-block p-0" />
+          </a>
+          
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <div className='ms-auto'>
-          <a className="navbar-brand img-fluid p-0 d-none d-lg-block" href="#">
-            <img src={Logo} alt="" width="200" class="p-0" />
-          </a>
+            <a className="navbar-brand img-fluid p-0 d-none d-lg-block" href="#">
+              <img src={'Logo'} alt="" width="200" class="p-0" />
+            </a>
           </div>
         </div>
       </div>
